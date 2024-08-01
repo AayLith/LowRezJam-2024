cd18445ab6fdfd33f3946e15ca251a0b3b267d0e
vuxee2 <vvlajic51@gmail.com>
2024-08-01 19:48:40 +0200

    Player, cam follow

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    
+    public float jumpForce = 50;
+    public LayerMask groundLayer;
+    private float checkDistance = 5; // Distance to check for ground
+
+    private Rigidbody2D rb;
+    private float moveInputX;
+    
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        moveInputX = Input.GetAxisRaw("Horizontal");
+        
+        if(Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            Jump();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInputX * moveSpeed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, checkDistance, groundLayer);
+        if (hit.collider != null)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    /*void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * checkDistance);
+    }*/
+}
