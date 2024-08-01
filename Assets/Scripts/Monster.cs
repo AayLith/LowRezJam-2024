840c557533ff4c47c5abbc7a4f06b12802cdde4b
AayLith <bzh-artikodin@hotmail.fr>
2024-08-01 17:35:49 +0200

    Init

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Monster : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static LinkedList<Monster> allMonsters = new LinkedList<Monster> ();
 
-    // Update is called once per frame
-    void Update()
+    public float speed { get { return Mathf.Max ( 0 , _speed * speedmod ); } }
+    public float _speed = 10;
+    public float speedmod = 1;
+    public int pathindex = 0;
+    [HideInInspector] public float cur_health;
+    public float max_health;
+
+    private void Awake ()
     {
-        
+        pathindex = 0;
+        cur_health = max_health;
+        allMonsters.AddFirst ( this );
     }
 }
